ace66df2b1f7e8dfa6358464860cbb9b73f059e0
ishida-s1 <I-s-23shun9268@gmail.com@users.noreply.github.com>
2020-04-14 15:42:47 +0900

    Material-UIのButton、Tableなどを追加。styled-componentsの適応

--- src/components/calender.tsx
+++ src/components/calender.tsx
@@ -9,6 +9,26 @@ import addMonths from "date-fns/addMonths";
 import subMonths from "date-fns/subMonths";
 import startOfMonth from "date-fns/startOfMonth";
 import endOfMonth from "date-fns/endOfMonth";
+import styled from "styled-components";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+
+// Material-UI をカッコで囲んで、styled の引数にしてやる
+const StyledButton = styled(Button)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border-radius: 3px;
+  border: 0;
+  color: white;
+  height: 48px;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+`;
 
 const getCalendarArray = (date: Date): Date[][] => {
   const sundays = eachWeekOfInterval({
@@ -28,45 +48,48 @@ const calendar: React.FC = () => {
   return (
     <div>
       <div>
-        <button
+        <StyledButton
           type="button"
           onClick={(): void => setTargetDate(current => subMonths(current, 1))}
         >
           前の月
-        </button>
-        <button type="button" onClick={(): void => setTargetDate(new Date())}>
+        </StyledButton>
+        <StyledButton
+          type="button"
+          onClick={(): void => setTargetDate(new Date())}
+        >
           今月
-        </button>
-        <button
+        </StyledButton>
+        <StyledButton
           type="button"
           onClick={(): void => setTargetDate(current => addMonths(current, 1))}
         >
           次の月
-        </button>
+        </StyledButton>
       </div>
       {format(targetDate, "y年M月")}
-      <table>
-        <thead>
-          <tr>
-            <th>日</th>
-            <th>月</th>
-            <th>火</th>
-            <th>水</th>
-            <th>木</th>
-            <th>金</th>
-            <th>土</th>
-          </tr>
-        </thead>
-        <tbody>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>日</TableCell>
+            <TableCell>月</TableCell>
+            <TableCell>火</TableCell>
+            <TableCell>水</TableCell>
+            <TableCell>木</TableCell>
+            <TableCell>金</TableCell>
+            <TableCell>土</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {oneMonth.map((weekRow, rowNum) => (
-            <tr key={rowNum.toString()}>
+            <TableRow key={rowNum.toString()}>
               {weekRow.map(date => (
-                <td key={getDay(date)}>{getDate(date)}</td>
+                <TableCell key={getDay(date)}>{getDate(date)}</TableCell>
               ))}
-            </tr>
+            </TableRow>
           ))}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
     </div>
   );
 };

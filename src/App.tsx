386c2fa6c181cb840fd96b8eca5e772e9433caaa
ishida-s1 <I-s-23shun9268@gmail.com@users.noreply.github.com>
2020-04-14 12:14:08 +0900

    Titleにstyled-componentsを適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import styled from "styled-components";
 import Calender from "./components/calender";
+
+// Title コンポーネントの作成、中身は h1 タグにスタイルを適応したもの
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1>START</h1>
+      <Title>START</Title>
       <Calender />
     </div>
   );

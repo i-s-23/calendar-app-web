3a734567cca03634a81c4058abdc9d726c93a300
ishida-s1 <I-s-23shun9268@gmail.com@users.noreply.github.com>
2020-04-16 15:33:25 +0900

    下記を参考にCSSの動的変更と各要素にstyled-componentsを追加。CSS部分に不具合が残っている 'http://yucatio.hatenablog.com/entry/2019/12/26/081346'

--- src/components/calender.tsx
+++ src/components/calender.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import format from "date-fns/format";
 import getDate from "date-fns/getDate";
 import getDay from "date-fns/getDay";
@@ -10,16 +10,22 @@ import subMonths from "date-fns/subMonths";
 import startOfMonth from "date-fns/startOfMonth";
 import endOfMonth from "date-fns/endOfMonth";
 import styled from "styled-components";
+import isSameMonth from "date-fns/isSameMonth";
+import isSameDay from "date-fns/isSameDay";
+import blue from "@material-ui/core/colors/blue";
+import red from "@material-ui/core/colors/red";
+import pink from "@material-ui/core/colors/pink";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 
-// Material-UI をカッコで囲んで、styled の引数にしてやる
+// Material-UI のButtonをカッコで囲んで、styled の引数にしてCSS in JSを実現している.
 const StyledButton = styled(Button)`
   background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
   border-radius: 3px;
@@ -30,6 +36,72 @@ const StyledButton = styled(Button)`
   box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
 `;
 
+const StyledPaper = styled(Paper)`
+  margin: (5, 10);
+  padding: (5, 5);
+`;
+
+const StyleTypography = styled(Typography)`
+  text-align: center;
+  margin: (2, 0, 1, 0);
+`;
+
+const StyleTableHeadCell = styled(TableCell)`
+  text-align: center;
+`;
+
+interface Props {
+  wday: Number;
+  isTargetMonth: Boolean;
+  istargetday: boolean;
+  children: any;
+}
+
+// Propsで扱っている情報を引数として日付として取得し、表示用のstyled-componentsに格納
+function CalendarTableCell(props: Props) {
+  const { wday, isTargetMonth, istargetday, children, ...other } = props;
+  return (
+    <StyleTableCell
+      wday={props.wday}
+      isTargetMonth={props.isTargetMonth}
+      istargetday={props.istargetday}
+  >{props.children}</StyleTableCell>
+  );
+}
+
+// 引数に応じてCellの色を変化させる。対象月、本日、土日と平日などで色分け
+const StyleTableCell = styled(TableCell)<{
+  wday: Number;
+  isTargetMonth: Boolean;
+  istargetday: boolean;
+}>`
+  text-align: center;
+  color: ${({ wday, isTargetMonth }): string => {
+    if (isTargetMonth) {
+      switch (wday) {
+        case 0: // Sunday
+          return red[500];
+        case 6: // Saturday
+          return blue[500];
+        default:
+          return "black";
+      }
+    } else {
+      // previous or next month
+      switch (wday) {
+        case 0: // Sunday
+          return red[200];
+        case 6: // Saturday
+          return blue[200];
+        default:
+          return "black";
+      }
+    }
+  }};
+  background: ${(istargetday): string => (istargetday ? pink[50] : blue[500])};
+`;
+
+// カレンダーに表示するための一月分のArrayを返す関数
 const getCalendarArray = (date: Date): Date[][] => {
   const sundays = eachWeekOfInterval({
     start: startOfMonth(date),
@@ -40,56 +112,73 @@ const getCalendarArray = (date: Date): Date[][] => {
   );
 };
 
+// カレンダーを表示するReactのFunctionComponent
 const calendar: React.FC = () => {
   const [targetDate, setTargetDate] = useState(new Date());
 
   const oneMonth = getCalendarArray(targetDate);
+  const weekdays = ["日", "月", "火", "水", "木", "金", "土"];
 
   return (
     <div>
-      <div>
-        <StyledButton
-          type="button"
-          onClick={(): void => setTargetDate(current => subMonths(current, 1))}
-        >
-          前の月
-        </StyledButton>
-        <StyledButton
-          type="button"
-          onClick={(): void => setTargetDate(new Date())}
-        >
-          今月
-        </StyledButton>
-        <StyledButton
-          type="button"
-          onClick={(): void => setTargetDate(current => addMonths(current, 1))}
-        >
-          次の月
-        </StyledButton>
-      </div>
-      {format(targetDate, "y年M月")}
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>日</TableCell>
-            <TableCell>月</TableCell>
-            <TableCell>火</TableCell>
-            <TableCell>水</TableCell>
-            <TableCell>木</TableCell>
-            <TableCell>金</TableCell>
-            <TableCell>土</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {oneMonth.map((weekRow, rowNum) => (
-            <TableRow key={rowNum.toString()}>
-              {weekRow.map(date => (
-                <TableCell key={getDay(date)}>{getDate(date)}</TableCell>
+      <StyledPaper>
+        <Grid container justify="space-between">
+          <Grid item>
+            <StyledButton
+              type="button"
+              onClick={(): void =>
+                setTargetDate(current => subMonths(current, 1))
+              }
+            >
+              前の月
+            </StyledButton>
+          </Grid>
+          <Grid item>
+            <StyledButton
+              type="button"
+              onClick={(): void => setTargetDate(new Date())}
+            >
+              今月
+            </StyledButton>
+          </Grid>
+          <Grid item>
+            <StyledButton
+              type="button"
+              onClick={(): void =>
+                setTargetDate(current => addMonths(current, 1))
+              }
+            >
+              次の月
+            </StyledButton>
+          </Grid>
+        </Grid>
+        <StyleTypography>{format(targetDate, "y年M月")}</StyleTypography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {weekdays.map(day => (
+                <StyleTableHeadCell key={day}>{day}</StyleTableHeadCell>
               ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {oneMonth.map((weekRow, rowNum) => (
+              <TableRow key={rowNum.toString()}>
+                {weekRow.map(date => (
+                  <CalendarTableCell
+                    key={getDay(date)}
+                    wday={getDay(date)}
+                    isTargetMonth={isSameMonth(date, targetDate)}
+                    istargetday={isSameDay(date, targetDate)}
+                  >
+                    {getDate(date)}
+                  </CalendarTableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </StyledPaper>
     </div>
   );
 };
